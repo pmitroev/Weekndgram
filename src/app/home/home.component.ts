f4b5eed743a559b51a6a3976438584848ee11c41
Preslav Mitroev <presalv1233@gmail.com>
2023-08-15 10:49:19 +0300

    set up the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { DataService } from '../shared/dataService/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private data: DataService) {}
 
+  posts: any = [];
+
+  refreshPosts() {
+    this.data.getPosts().subscribe((res) => {
+      this.posts = res;
+      this.posts = this.posts.slice(0,3);
+    });
+  }
+
+  ngOnInit() {
+    this.refreshPosts();
+  }
 }
