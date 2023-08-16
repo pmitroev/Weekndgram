7bc879c297d9eb43a69dc454890f0625baa13c76
Preslav Mitroev <presalv1233@gmail.com>
2023-08-16 17:27:33 +0300

    updating post model

--- src/app/types/post.ts
+++ src/app/types/post.ts
@@ -1,11 +1,7 @@
-import { User } from "./user";
-
 export interface Post {
     _id: string;
     place: string;
     description: string;
     imageUrl: string;
-    userId: string;
-    likes: string[];
-    user: User;
+    uid: string;
 }
