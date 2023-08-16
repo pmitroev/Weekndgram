2dfdda6b48827b69e4de048d118cf0ddff2bf8ac
Preslav Mitroev <presalv1233@gmail.com>
2023-08-16 17:28:48 +0300

    implementing validators

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AddPostComponent } from './add-post/add-post.component';
 import { PostRoutingModule } from './post-routing.module';
 import { CurrentPostComponent } from './current-post/current-post.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AddPostComponent, CurrentPostComponent],
-  imports: [CommonModule, PostRoutingModule, FormsModule],
+  imports: [CommonModule, PostRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class PostModule {}
