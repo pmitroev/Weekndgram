84835fe2b7b4e14307266b8af52610f5499e59d7
Preslav Mitroev <presalv1233@gmail.com>
2023-08-16 17:28:38 +0300

    adding validator for creating a post

--- src/app/post/add-post/add-post.component.ts
+++ src/app/post/add-post/add-post.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Firestore, collection, addDoc, setDoc, doc } from '@angular/fire/firestore';
-import { NgForm } from '@angular/forms';
+import {
+  Firestore,
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+} from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,17 +16,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-post.component.css'],
 })
 export class AddPostComponent {
-  constructor(private firestore: Firestore, private router: Router, private auth: AngularFireAuth) {}
+  postForm: FormGroup;
 
+  constructor(
+    private firestore: Firestore,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.postForm = this.fb.group({
+      place: ['', Validators.required],
+      description: ['', Validators.required],
+      imageUrl: ['', [Validators.required, Validators.pattern('https?://.*')]],
+    });
+  }
+  place: string = '';
+  description: string = '';
+  imageUrl: string = '';
   addData(form: NgForm) {
+    if (this.place == '') {
+        alert('Please enter a place');
+        return;
+    }
 
+    if (this.description == '') {
+        alert('Please enter a description');
+        return;
+    }
+
+    if (this.imageUrl == '') {
+        alert('Please enter an imageUrl');
+        return;
+    }
     const postsInstance = collection(this.firestore, 'posts');
 
-    addDoc(postsInstance, {...form.value, uid: localStorage.getItem('token')}).then(() => {
-        this.router.navigate(['/posts'])
-    })
-    .catch((err) => {
+    addDoc(postsInstance, { ...form.value, uid: localStorage.getItem('token') })
+      .then(() => {
+        this.router.navigate(['/posts']);
+      })
+      .catch((err) => {
         console.log(err);
-    })
+      });
   }
 }
