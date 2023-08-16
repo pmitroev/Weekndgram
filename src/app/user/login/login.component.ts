e5d8ed94ebe7545ce6e731fb107ed9e65e53cce5
Preslav Mitroev <presalv1233@gmail.com>
2023-08-16 17:29:00 +0300

    validating

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,34 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-    
-    constructor(private auth: AuthService) { }
-    ngOnInit(): void {
-        // this.auth.login('preslav@gmail.com', '123456');
-    }
-    
-    email : string = '';
-    password : string = '';
-    login(): void {
-        if (this.email == '') {
-            alert('Please enter an email');
-            return;
-        }
+export class LoginComponent {
+  loginForm: FormGroup;
 
-        if (this.password == '') {
-            alert('Please enter a password');
-            return;
-        }
+  constructor(private auth: AuthService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
-        this.auth.login(this.email, this.password);
-        this.email = '';
-        this.password = '';
+  email: string = '';
+  password: string = '';
+  login(): void {
+    if (this.email == '') {
+      alert('Please enter an email');
+      return;
     }
+
+    if (this.password == '') {
+      alert('Please enter a password');
+      return;
+    }
+
+    this.auth.login(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
 }
